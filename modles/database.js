4f7eeb688baddbddf67f1fd1cd561c24df3a36a1
agruenth <agruenth@stud.hs-heilbronn.de>
2018-04-11 09:20:46 +0200

    Database connection established, test query, setup to local to config

---
+++ modles/database.js
@@ -0,0 +1,25 @@
+const {Pool} = require('pg');
+
+
+const pgUrl = require('pg-database-url');
+
+
+const dbConfig = {
+    host: '192.168.99.100',
+    port: 5432,
+    database: 'postgres',
+    username: 'postgres',
+    password: 'admin'
+};
+
+const connectionString = pgUrl(dbConfig);
+
+const pool = new Pool({
+    connectionString: connectionString,
+});
+
+pool.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)', (err, res) => {
+    console.log(err, res);
+    pool.end()
+});
+
