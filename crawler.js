925412ca04812e43c51310d614f2ae1ca87b97a6
Bastian Knaup <bknaup@stud.hs-heilbronn.de>
2018-04-12 12:13:32 +0200

    Habe die crawler.js erstellt Habe die notwendigen Node Modules installiert

---
+++ crawler.js
@@ -0,0 +1,8 @@
+/* request für HTTP requests -
+cheerio zum parsen der HTML Elemente -
+URL um URLs zu parsen
+ */
+
+var request = require('request')
+var cheerio = require('cheerio')
+var URL = require('url-parse')
