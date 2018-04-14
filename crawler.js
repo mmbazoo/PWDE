443aeb45bca453dbdcd978fb3d162c3fd714dda8
Enes Can Sendal <esendal@stud.hs-heilbronn.de>
2018-04-14 14:13:29 +0200

    Produktinformationen werden jetzt in einem Array, der aus Arrays besteht gespeichert.

--- crawler.js
+++ crawler.js
@@ -12,50 +12,42 @@ var cheerio = require('cheerio');
 var URL = require('url-parse');
 
 
-/* Fetching engelhorn.de
-bzw. die Kategorie Hemden
- */
+// Fetching engelhorn.de bzw. die Kategorie Hemden
 
-var page_to_visit = "http://www.engelhorn.de/fashion/herren/bekleidung/hemden";
+
+var page_to_visit = "http://www.engelhorn.de/fashion/herren/bekleidung/strickjacken";
 console.log("Besuchte Seite " + page_to_visit);
 request(page_to_visit, function(error, response, body) {
 
-    // Fehlermeldung, falls es zu einem Error kommt
-    if(error) {
-        console.log("Error: " + error);
-    }
+  // Fehlermeldung, falls es zu einem Error kommt
+  if(error) {
+    console.log("Error: " + error);
+  }
 
-    // Statuscode checken (200 -> HTTP OK)
-    console.log("Status code: " + response.statusCode);
-    if(response.statusCode === 200) {
+  // Statuscode checken (200 -> HTTP OK)
+  console.log("Status code: " + response.statusCode);
+  if(response.statusCode === 200) {
 
-        // Parsen des Body´s - speichern in Variable body_elements
-        var body_elements = cheerio.load(body);
+    // Parsen des Body´s - speichern in Variable $
+    var $ = cheerio.load(body);
 
-        // Titel der Seite auslesen der in der Variable body_elements gespeichert ist
-        console.log("Seitentitel:  " + body_elements('title').text());
+    // Titel der Seite auslesen der in der Variable body_elements gespeichert ist
+    console.log("Seitentitel:  " + $('title').text());
+
+    var products = []; // Leeren Array für ALLE Produkte anlegen
+    $(".product-tile").each(function(){ // Funktion für jedes Produkt ausführen
+      var product = []; // Leeren Array für EINZELNES Produkt anlegen
+      // Produktinformationen extrahieren, ggf. formatieren und in den Array pushen
+      var product_id = $(this).attr("data-itemid");
+      var product_price = $(this).find('div > div.product-information > div.product-price > span.price-sales').text().replace(/(\r\n\t|\n|\r\t)/gm,"");
+      var product_brand = $(this).find('div > div.product-information > div.product-name > div.brand').text().replace(/(\r\n\t|\n|\r\t)/gm,"");
+      var product_name = $(this).find('div > div.product-information > div.product-name > div.name > a').text().replace(/(\r\n\t|\n|\r\t)/gm,"");
+      var current_time = external_functions.current_time();
+      product.push(product_id, product_price, product_brand,product_name, current_time);
+      products.push(product); // Produkt Array des einzelnen Produktes in Produkt Array aller Produkte pushen
+    });
+    console.log(products);
 
 
-        /* Alle notwendigen Informationen werden nun abgefragt
-        Preise - Bezeichnungen - normale Preise - Sale Preise - Datum
-         */
-
-        // Alle Preise mit regex
-        var prices = (body_elements('.product-price span').text());
-        var prices_regex = prices.replace(/\s\s+/g, ' ');
-        console.log("Alle Preise: \n" + prices_regex);
-
-        // Alle Marken mit regex
-        var brands = (body_elements('.brand').text());
-        var brands_regex = brands.replace(/\s\s+/g, ' ');
-        console.log("Alle Marken: \n" + brands_regex);
-
-        // Alle Bezeichnungen mit regex
-        var names = (body_elements('.name-link').text());
-        var names_regex = names.replace(/\s\s+/g, ' ');
-        console.log("Alle Bezeichnungen: \n" + names_regex);
-
-        // Aktuelles Datum
-        console.log("Aktuelles Datum: " + external_functions.current_time());
-    }
+  }
 });
