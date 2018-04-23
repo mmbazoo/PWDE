dff43e4c7a0a276d8a4d9c93a8cfd7396eb78f3f
Alexander Brechlin <brechlin@stud.hs-heilbronn.de>
2018-04-23 17:59:33 +0200

    Testcommit mit geänderter Datenbankverbindung zum CI-Server und initiales Erstellen der Tabellen in der CI Datenbank

--- database.js
+++ database.js
@@ -1,38 +1,57 @@
 const {Pool} = require('pg');
-const config = require('config');
+//const config = require('./config');
+//const connectionString = config.connectionString;
+
+const pgUrl = require('pg-database-url');
+
+const config = {};
+
+const dbConfig = {
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'postgres',
+    username: 'postgres',
+    password: 'admin'
+};
+
+config.connectionString = pgUrl(dbConfig);
 const connectionString = config.connectionString;
 
-function create_db(){
-    const pool = new Pool({
-        connectionString: connectionString,
-    });
+module.exports = {
+    create_db:function () {
+        const pool = new Pool({
+            connectionString: connectionString,
+        });
 
-    pool.query('CREATE TABLE IF NOT EXISTS Produkt(artikelnummer VARCHAR(50) NOT NULL,' +
-        ' bezeichnung TEXT NULL,' +
-        ' marke VARCHAR(45) NULL,' +
-        ' URL TEXT NULL,' +
-        ' PRIMARY KEY (artikelnummer));' +
-        '' +
-        ' CREATE TABLE IF NOT EXISTS Preise(' +
-        ' ID_preise SERIAL, artikelnummer VARCHAR(50) NULL,' +
-        ' preis FLOAT NULL,' +
-        ' zeitstempel TIMESTAMP(0) NULL,' +
-        ' PRIMARY KEY (ID_preise));' +
-        '' +
-        ' CREATE TABLE IF NOT EXISTS Kategorie(' +
-        ' ID_kategorie SERIAL,' +
-        ' kategorie VARCHAR(45) NULL,' +
-        ' rang INT NULL,' +
-        '' +
-        ' PRIMARY KEY (ID_kategorie));' +
-        ' CREATE TABLE IF NOT EXISTS Matching_kategorie(' +
-        ' ID_matching SERIAL,' +
-        ' artikelnummer VARCHAR(50) NOT NULL,' +
-        ' ID_kategorie INT NULL,' +
-        ' PRIMARY KEY (ID_matching))', (err, res) => {
-        console.log(err, res);
-        pool.end()
-    });
-}
+        pool.query('CREATE TABLE IF NOT EXISTS Produkt(artikelnummer VARCHAR(50) NOT NULL,' +
+            ' bezeichnung TEXT NULL,' +
+            ' marke VARCHAR(45) NULL,' +
+            ' URL TEXT NULL,' +
+            ' PRIMARY KEY (artikelnummer));' +
+            '' +
+            ' CREATE TABLE IF NOT EXISTS Preise(' +
+            ' ID_preise SERIAL, artikelnummer VARCHAR(50) NULL,' +
+            ' preis FLOAT NULL,' +
+            ' zeitstempel TIMESTAMP(0) NULL,' +
+            ' PRIMARY KEY (ID_preise));' +
+            '' +
+            ' CREATE TABLE IF NOT EXISTS Kategorie(' +
+            ' ID_kategorie SERIAL,' +
+            ' kategorie VARCHAR(45) NULL,' +
+            ' rang INT NULL,' +
+            '' +
+            ' PRIMARY KEY (ID_kategorie));' +
+            ' CREATE TABLE IF NOT EXISTS Matching_kategorie(' +
+            ' ID_matching SERIAL,' +
+            ' artikelnummer VARCHAR(50) NOT NULL,' +
+            ' ID_kategorie INT NULL,' +
+            ' PRIMARY KEY (ID_matching))', (err, res) => {
+            console.log(err, res);
+            pool.end()
+        });
+        console.log("Die Funktion wurde aufgerufen");
+    }
+    };
+
 
 
