9df8dcb88f15417d3f4af8e9ddf695cf0f19661c
Alexander Brechlin <brechlin@stud.hs-heilbronn.de>
2018-04-23 14:32:13 +0200

    YAML File geändert. SQL für die entwickelte Datenstruktur erstellt und auf lokalen Testserver getestet

--- models/database.js
+++ models/database.js
@@ -16,7 +16,29 @@ const pool = new Pool({
     connectionString: connectionString,
 });
 
-pool.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)', (err, res) => {
+pool.query('CREATE TABLE IF NOT EXISTS Produkt(artikelnummer VARCHAR(50) NOT NULL,' +
+    ' bezeichnung TEXT NULL,' +
+    ' marke VARCHAR(45) NULL,' +
+    ' URL TEXT NULL,' +
+    ' PRIMARY KEY (artikelnummer));' +
+    '' +
+    ' CREATE TABLE IF NOT EXISTS Preise(' +
+    ' ID_preise SERIAL, artikelnummer VARCHAR(50) NULL,' +
+    ' preis FLOAT NULL,' +
+    ' zeitstempel TIMESTAMP(0) NULL,' +
+    ' PRIMARY KEY (ID_preise));' +
+    '' +
+    ' CREATE TABLE IF NOT EXISTS Kategorie(' +
+    ' ID_kategorie SERIAL,' +
+    ' kategorie VARCHAR(45) NULL,' +
+    ' rang INT NULL,' +
+    '' +
+    ' PRIMARY KEY (ID_kategorie));' +
+    ' CREATE TABLE IF NOT EXISTS Matching_kategorie(' +
+    ' ID_matching SERIAL,' +
+    ' artikelnummer VARCHAR(50) NOT NULL,' +
+    ' ID_kategorie INT NULL,' +
+    ' PRIMARY KEY (ID_matching))', (err, res) => {
     console.log(err, res);
     pool.end()
 });
