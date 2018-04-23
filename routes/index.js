ab2eb1c13a5b51360d7006dca7ecf1d773045de0
agruenth <agruenth@stud.hs-heilbronn.de>
2018-04-23 16:27:59 +0200

    test of new CI approach to db, with || true

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {Pool} = require('pg');
-const config = require('../config');
-const connectionString = config.connectionString;
-
-const pool = new Pool({
-    connectionString: connectionString,
-});
 
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', {title: 'Express'});
-});
-
-/* Test Query */
-const query = {
-    text: 'INSERT INTO items(id, text) VALUES($1, $2)',
-    values: ['2', 'brian.m.carlson@gmail.com'],
-};
-
-// callback
-pool.query(query, (err, res) => {
-    if (err) {
-        console.log(err.stack)
-    } else {
-        console.log(res.rows[0])
-    }
 });
 
 module.exports = router;
